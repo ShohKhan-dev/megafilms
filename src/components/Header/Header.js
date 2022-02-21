1cfa6410b74fc9910dc3ad81ed5501748e261cee
Shohjahon <shoh.hamraqulov@gmail.com>
2022-02-21 11:07:19 +0300

    add header link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <span onClick={() => window.scroll(0, 0)} className="header">
-      🎬 Megafilms 🎬
-    </span>
+    <Link to="/" className="actor-img">
+      <span className="header">
+        🎬 Megafilms 🎬
+      </span>
+    </Link>
+    
   );
 };
 
